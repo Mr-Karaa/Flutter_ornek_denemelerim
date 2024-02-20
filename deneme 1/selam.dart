f9f2d1a9cd35aba6b3a4f06cf62e54104e71be8d
Mr-Karaa <karamelih315@gmail.com>
2024-02-20 13:51:32 +0300

    yorum satirlari inaktif

--- deneme 1/selam.dart
+++ deneme 1/selam.dart
@@ -1,25 +1,25 @@
 
 
 void main(List<String> args) {
-  /*print("1.notu gir gir");
+  print("1.notu gir gir");
   int not1 = int.parse(stdin.readLineSync()!);
   print("2.notu gir");
   int not2 = int.parse(stdin.readLineSync()!);
   double sonuc = (not1 + not2)/2;
-  print("sonucunuz $sonuc");*/
+  print("sonucunuz $sonuc");
 
-  /*print("programa hosgeldinn ♥");
+  print("programa hosgeldinn ♥");
   print("lutfen ilk notunu gir ;");
   int not1 = int.parse(stdin.readLineSync()!);
   print("not 1 $not1");
   int not2 = int.parse(stdin.readLineSync()!);
   print("not2 $not2'dir onayliyormsunuz:");
   double cevap = not1 + not2 / 2;
-  print("ortalama $cevap");*/
+  print("ortalama $cevap");
 
-  /*print(hacimHesapla(8, 9, 10));*/
+  print(hacimHesapla(8, 9, 10));
 }
 
-/*int hacimHesapla(kenar1, kenar2, yukseklik) {
+int hacimHesapla(kenar1, kenar2, yukseklik) {
   return kenar1 * kenar2 * yukseklik;
-}*/
+}
