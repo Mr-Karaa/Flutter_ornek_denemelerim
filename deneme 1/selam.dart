26eb2f5aee9d46d77eb1ae35e1f05070f9e11481
Mr-Karaa <karamelih315@gmail.com>
2024-02-20 13:45:40 +0300

    bu bir deneme

---
+++ deneme 1/selam.dart
@@ -0,0 +1,25 @@
+
+
+void main(List<String> args) {
+  /*print("1.notu gir gir");
+  int not1 = int.parse(stdin.readLineSync()!);
+  print("2.notu gir");
+  int not2 = int.parse(stdin.readLineSync()!);
+  double sonuc = (not1 + not2)/2;
+  print("sonucunuz $sonuc");*/
+
+  /*print("programa hosgeldinn ♥");
+  print("lutfen ilk notunu gir ;");
+  int not1 = int.parse(stdin.readLineSync()!);
+  print("not 1 $not1");
+  int not2 = int.parse(stdin.readLineSync()!);
+  print("not2 $not2'dir onayliyormsunuz:");
+  double cevap = not1 + not2 / 2;
+  print("ortalama $cevap");*/
+
+  /*print(hacimHesapla(8, 9, 10));*/
+}
+
+/*int hacimHesapla(kenar1, kenar2, yukseklik) {
+  return kenar1 * kenar2 * yukseklik;
+}*/
