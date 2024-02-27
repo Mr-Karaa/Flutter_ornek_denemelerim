5af77bd47fd11ebf75d92b31dfa3179798ccb47b
Mr-Karaa <karamelih315@gmail.com>
2024-02-27 10:17:13 +0300

    aciklama yapildi dersde neler islnecek belirtildi

--- deneme 1/nesne yonelimli/sinifvenesne.dart
+++ deneme 1/nesne yonelimli/sinifvenesne.dart
@@ -1,4 +1,11 @@
 //sinif ve nesne kavramlarinin ornegi
 void main(List<String> args) {
-  
+  int sayi = 7; // int dedik ve sayi tuttuk
+  String isim = 'melih'; //string dedik kelimme tuttuk
+  List<int> liste = []; //ve liste ile de bazi yapilari siniflandirdik
+  print(sayi);
+  print(isim);
+  print(liste);
+
+  // iste bugun de aslinda yapacagimiz sey siniflandirma olaack bu dersimizde class yapilarini gorcez
 }
