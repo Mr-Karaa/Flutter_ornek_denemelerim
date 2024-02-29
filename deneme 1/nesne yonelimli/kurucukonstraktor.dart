06c9ca00dcbf132a6f15a90de6e8b050e2190427
Mr-Karaa <karamelih315@gmail.com>
2024-02-29 13:19:17 +0300

    anahtar kelime kullanimi gosterildi

--- deneme 1/nesne yonelimli/kurucukonstraktor.dart
+++ deneme 1/nesne yonelimli/kurucukonstraktor.dart
@@ -1,34 +1,38 @@
+import 'dart:async';
 void main(List<String> args) {
   //ilk once taslagimizi olusturalim
   //class araba taslak orayi incele ilk once
-
-  Araba mercedes =
- Araba(1998,"benz", true); 
-      
-      //boyle diyerek kutucugu olusturduk simdi bunun icerisne gerekn bilgileri ekleyelim
-
-
+  Araba mercedes = Araba(1998, "mercedes w124 series", false);
+  //boyle diyerek kutucugu olusturduk simdi bunun icerisne gerekn bilgileri ekleyelim
 /*   mercedes.modelYili =   //1. yol olarak bu kullanilabilir kolaylik olarak ta
       1998; //burada mercede "." diyerek yazmis oldugumuz model yili marka ve manuelmi bilgilreini dolduralim
   mercedes.marka = "benz";
   mercedes.manuelMi = false; */
-
-
-
   mercedes
       .bilgileriSoyle(); //yazarak terminalde yaptigimiz islemleri gorebilirz ayni zamand adiyelim guncelleme yapmak sitedik:
   mercedes.manuelMi = true; //guncel bilgiyi yazip
+  print("bilgiler guncelsstirildi");
   mercedes.bilgileriSoyle(); // yazdirirsak (f5)
   //bu sekilde de guncelleme yapabilir bilgileri degistirebilirz
-  var renault =
-      Araba(null ,null ,null); //mesela burayi null birakalim tam olrak nasilmis gorelim
+  var renault = Araba(2012, "renault clio",
+      false); //mesela burayi null birakalim tam olrak nasilmis gorelim
   renault.bilgileriSoyle();
+  var bmw = Araba(2021, "bmw i series", false);
+  print(
+      "////////////////"); //burada aslinda altaki yas hesapla metotdunu calistirdik
+  bmw.yasHesapla();
+  print(
+      "////////////////"); //burada aslinda altaki yas hesapla metotdunu calistirdik
+  mercedes.yasHesapla();
+  print(
+      "////////////////"); //burada aslinda altaki yas hesapla metotdunu calistirdik
+  renault.yasHesapla();
+  print(
+      "////////////////"); //burada aslinda altaki yas hesapla metotdunu calistirdik
 }
-
 //peki hersey tamamsa kurucu metot nedir
 //kurucu metot biz gormesek te burada ki Araba() satirnin orada acilan bir gorunmez metot kanitlayada biliriz
 //void bilgileri soyleden oncedeki satira bak
-
 //simdi burada naptik
 class Araba {
   // araba adinda bir sinif olusturduk
@@ -36,19 +40,27 @@ class Araba {
   String? marka; //yine null olarak markasini ve
   bool?
       manuelMi; //null olarak maunel mi otomatik mi bu bilgileri sinifimiz icerisinde taslak olrak hazirladik
-
 /*   Araba() {      //1.yontem
     print(
         "kurucu metot baslaitldi"); //bu sekilde bir kanit sunabilirim simdi f5 yaparsak aslinda class araba dan once burasi calisir deneyelim ...
   } */
-  Araba(int? modelYili, String? marka, bool? manuelMi) {
-    print("object");
-  }// 2. yontem bu sekilde de yazarak kolaylik elde edebilriz
-
+  Araba(this.modelYili, this.marka, this.manuelMi) {
+    print("konsol aktif");
+  }
+  // Araba(int? modelYili, String? marka, bool? manuelMi) { //anahtar kelime kullaniraken this yontemi bu sekilde kullanilir diger yontem ise ustte
+  //   this.modelYili = modelYili;
+  //   this.manuelMi = manuelMi;
+  //   this.marka = marka;
+  //   print("kurucu metot baslatildi");
+  // } // 2. yontem bu sekilde de yazarak kolaylik elde edebilriz
   void bilgileriSoyle() {
     //bilgileri soyle diye bir metot olusturduk
     print(
         "model yili: ${modelYili} , markasi: ${marka} , manuel mi: ${manuelMi}"); // ve printle bu bilgileri yazdiracagimiz satiri eklemis olduk
     //simdi iskeletimiz tamam void mainin icerisine girip tabiri caizsse uretime gecelim klafjakjfajflka
+  }
+  void yasHesapla() {
+    print(
+        ' $marka arabasinin yasi ${2024 - modelYili!}'); //unlem koymamizin sebebi burada ki deger null olabilir diyo ve bizi uyariyor bizde unlem koyarak diyoruz ki rahat ol kardesim sen null deger almayacaksin.
   }
 }
